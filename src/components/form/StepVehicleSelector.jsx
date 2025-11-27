e8f5728f43c4bb5fb8473d3d3ca0c7a5e2f64f74
simoneburrai <burrai.s98@gmail.com>
2025-11-27 22:06:12 +0100

    feat: filteredVehicles with useMemo, select on vehicleType in StepVehicleSelector

--- src/components/form/StepVehicleSelector.jsx
+++ src/components/form/StepVehicleSelector.jsx
@@ -2,6 +2,7 @@ import FormControllerButtons from "./FormControllerButtons";
 import VehicleCard from "./VehicleCard";
 import { useTranslation } from "react-i18next";
 import { useGlobalContext } from "../../context/GlobalContext";
+import { useMemo, useState } from "react";
 
 export default function StepVehichleSelector({
   stepsLength,
@@ -12,6 +13,15 @@ export default function StepVehichleSelector({
   const { t } = useTranslation();
 
   const {error, vehicles, isLoading} = useGlobalContext();
+  const [selectTypeOption, setSelectTypeOption] = useState("");
+
+  const filteredVehicles = useMemo(()=>{
+    let currentVehicles = vehicles;
+    if(selectTypeOption){
+      currentVehicles = vehicles.filter(v => v.vehicleType === selectTypeOption);
+    }
+    return currentVehicles
+  }, [vehicles, selectTypeOption])
 
    if(error){
     return <h2 className="font-bold text-3xl text-center text-red-500">Errore: Nessun Veicolo trovato</h2>
@@ -27,7 +37,12 @@ export default function StepVehichleSelector({
       <h2 className="step-container-title">
         {t("veichleSelector.title")}
       </h2>
-
+      <div className="flex my-3">
+        <select className="ml-auto  px-3 py-2 border-2 border-primary bg-bg-default hover:bg-bg-alt rounded-lg shadow-lg text-text-default text-xl appearance-none cursor-pointer hover:border-primary-hover focus:outline-none" onChange={(e)=>setSelectTypeOption(e.target.value)} name="selectVehicleType" id="selectVehicleType">
+          <option value="">TUTTI</option>
+          {[...new Set(vehicles.map(v => v.vehicleType))].map((vT, i)=><option key={i} value={vT}>{vT.toUpperCase()}</option>)}
+        </select>
+      </div>
   
 
       <div
@@ -35,21 +50,21 @@ export default function StepVehichleSelector({
                 my-4
                 grow
                 flex items-start 
-                justify-center p-4 
+                justify-center py-4 
                 overflow-y-auto 
                 scrollbar-hide
                 max-h-[calc(100vh-200px)] 
             "
       >
         <div className="vehicle-card-container">
-          {vehicles.map((v) => {
+          {filteredVehicles.map((v) => {
             return (
               <VehicleCard key={v.id} vehicle={v}/>
             );
           })}
         </div>
       </div>
-      <div>
+      <div className="my-4">
           <FormControllerButtons props={{ stepsLength, currentStep, goToNextStep, goToPrevStep }}/>
       </div>
     </div>
